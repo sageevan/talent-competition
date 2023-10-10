3c6b708f1ac54d8261a89052dfde61bf5f54dec3
Sageevan Pathmaseelan <pathmasagee@gmail.com>
2023-10-10 21:40:54 +1300

    Working in lnaguages section

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/Language.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/Language.jsx
@@ -2,21 +2,24 @@
 import React from 'react';
 import Cookies from 'js-cookie';
 import { ChildSingleInput } from '../Form/SingleInput.jsx';
+import { BodyWrapper, loaderData } from '../Layout/BodyWrapper.jsx';
 
 export default class Language extends React.Component {
     constructor(props) {
         super(props);
-        const languagedata = props.languageData ?
-            Object.assign([], props.languageData)
-            : {
+        const languagedata = [
+            {
                 id:"",
                 name: "",
                 level :""
             }
+        ]
 
         this.state = {
+            loaderData: loaderData,
             addNew: false,
-            newLanguage: languagedata
+            newLanguage: languagedata,
+            languageData: languagedata
         }
         this.handleChange = this.handleChange.bind(this)
         this.renderAddNew = this.renderAddNew.bind(this)
@@ -24,8 +27,20 @@ export default class Language extends React.Component {
         this.addNew = this.addNew.bind(this)
         this.closeAddNew = this.closeAddNew.bind(this)
         this.saveLanguage = this.saveLanguage.bind(this)
+        this.init = this.init.bind(this)
         this.loadData = this.loadData.bind(this)
   
+    }
+    init() {
+        let loaderData = this.state.loaderData;
+        loaderData.allowedUsers.push("Talent");
+        loaderData.isLoading = false;
+        this.setState({ loaderData, })
+    }
+
+    componentDidMount() {
+        this.loadData();
+        this.init();
     }
     handleChange(event) {
         const data = Object.assign({}, this.state.newLanguage)
@@ -36,18 +51,18 @@ export default class Language extends React.Component {
     }
     addNew() {
         this.setState({ addNew: true })
-       // this.renderAddNew();
     }
     render() {
         return(
-        this.state.addNew ? this.renderAddNew() : this.renderDisplay()
-         //   this.renderDisplay()
+        this.state.addNew ? this.renderAddNew() : this.renderDisplay(this.state.languageData)
         )
     }
     closeAddNew() {
-        this.setState({ addNew:false })
+        this.setState({ addNew: false })
+
     }
     saveLanguage() {
+        this.loadData();
         const language = { 'name': this.state.newLanguage.name, 'level': this.state.newLanguage.level }
         // console.log(language)
         const data = Object.assign({}, language)
@@ -66,20 +81,23 @@ export default class Language extends React.Component {
             },
             type: "GET",
             success: function (res) {
-                //let profileData = null;
+                let languagedata = null;
                 if (res) {
-                    //profileData = res.data
-                    console.log("profileData", res)
+                    languagedata = res.languages
+                    console.log("profileData", res.languages)
                 }
-                console.log(res)
-                //this.updateWithoutSave(profileData)
+                console.log(res.languages)
+                this.setState({
+                    languageData: languagedata
+                })
             }.bind(this),
             error: function (res) {
                 console.log(res.status)
             }
         });
-       // this.init();
+        this.init();
     }
+
 
     renderAddNew() {
         return (
@@ -113,8 +131,8 @@ export default class Language extends React.Component {
         
     }
 
-    renderDisplay() {
-        this.loadData()
+    renderDisplay(languages) {
+       console.log(languages)
         return (
             <div className='language-table'>
                 <table class="ui table">
@@ -125,21 +143,20 @@ export default class Language extends React.Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>First</td>
-                            <td>Cell</td>
-                        </tr>
-                        <tr>
-                            <td>Cell</td>
-                            <td>Cell</td>
-                        </tr>
-                        <tr>
-                            <td>Cell</td>
-                            <td>Cell</td>
-                        </tr>
+                        {languages.map((language) => 
+                            <tr key={language.id}>
+                                <td>{language.language}</td>
+                                <td>{language.languageLevel}</td>
+                                </tr>
+           
+                        )}
                     </tbody>
                 </table>
             </div>
         )
     }
 }
+
+                                                        //<td><button className="btn-edit" onClick={() => { ctrl.updateCustomer(customer) }}><BsFillPencilFill /></button></td>
+                                    //<td><button className="btn-delete" onClick={() => { ctrl.deleteCustomerRequest(customer) }}><BsFillTrashFill /></button></td>
+
